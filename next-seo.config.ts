692a9fbe3a0a7f607614b24be9fb1d009a96a808
Onur Ucar <hello@ucaronur.com>
2024-07-17 22:28:54 -0400

    chore: Update next-seo npm dependency to version 6.5.0, added SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,21 @@
+import type { DefaultSeoProps } from 'next-seo'
+
+const APP_URL = 'TODO'
+const FULL_APP_URL = `https://${APP_URL}/`
+
+const defaultSEO: DefaultSeoProps = {
+  titleTemplate: `%s | ${APP_URL}`,
+  defaultTitle: 'ReadmeQuik',
+  description:
+    "",
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: FULL_APP_URL,
+    title: 'Readme Quik',
+    description: ``,
+    site_name: 'ReadmeQuik',
+  }
+}
+
+export default defaultSEO
