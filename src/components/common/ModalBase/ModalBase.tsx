600b1998a4707de006e411b124354d04c826b9d6
Onur Ucar <hello@ucaronur.com>
2024-07-17 22:25:29 -0400

    chore: refactored and cleared the use of atoms

--- src/components/common/ModalBase/ModalBase.tsx
+++ src/components/common/ModalBase/ModalBase.tsx
@@ -1,16 +1,16 @@
-import * as React from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { IconButton } from "~/components/primitives";
-import { FiX } from "react-icons/fi";
+import * as React from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { IconButton } from '~/components/primitives'
+import { FiX } from 'react-icons/fi'
 
 export interface IModalBaseProps {
-  isOpen: boolean;
-  handleClose: () => void;
-  children: React.ReactNode;
+  isOpen: boolean
+  handleClose: () => void
+  children: React.ReactNode
 }
 
 export function ModalBase({ isOpen, handleClose, children }: IModalBaseProps) {
-  const focusref = React.useRef(null);
+  const focusref = React.useRef(null)
   return (
     <Transition.Root show={isOpen} as={React.Fragment}>
       <Dialog
@@ -19,9 +19,8 @@ export function ModalBase({ isOpen, handleClose, children }: IModalBaseProps) {
         open={isOpen}
         onClose={() => handleClose()}
         initialFocus={focusref}
-        static={true}
       >
-        <div className="flex min-h-screen items-end justify-center px-4 pb-20 pt-4 text-center sm:block sm:p-0">
+        <div className="flex min-h-screen items-end justify-center px-4 pt-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child
             as={React.Fragment}
             enter="ease-out duration-300"
@@ -34,10 +33,7 @@ export function ModalBase({ isOpen, handleClose, children }: IModalBaseProps) {
             <Dialog.Overlay className="fixed inset-0  bg-neutral-focus bg-opacity-40 transition-opacity duration-200 ease-in-out" />
           </Transition.Child>
           {/* This element is to trick the browser into centering the modal contents. */}
-          <span
-            className="hidden sm:inline-block sm:h-screen sm:align-middle"
-            aria-hidden="true"
-          >
+          <span className="hidden sm:inline-block sm:h-screen sm:align-middle" aria-hidden="true">
             &#8203;
           </span>
           <Transition.Child
@@ -49,8 +45,8 @@ export function ModalBase({ isOpen, handleClose, children }: IModalBaseProps) {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="relative inline-block transform overflow-hidden rounded-lg bg-base-100 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle">
-              <div className="bg-base-100 px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div className="relative inline-block transform  rounded-lg bg-base-100 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle">
+              <div className="bg-base-100 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="flex justify-end p-2">
                   <IconButton
                     aria-label="close modal"
@@ -68,5 +64,5 @@ export function ModalBase({ isOpen, handleClose, children }: IModalBaseProps) {
         </div>
       </Dialog>
     </Transition.Root>
-  );
+  )
 }
--- src/components/editor/PreviewColumnTab/EditorColumnTabPanel.tsx
+++ src/components/editor/PreviewColumnTab/EditorColumnTabPanel.tsx
@@ -1,23 +1,24 @@
-import { useAtomValue } from "jotai";
-import * as React from "react";
-import { DraggableCard } from "~/components/editor";
-import { activeBlocksAtom } from "~/store";
+import { useAtomValue } from 'jotai'
+import * as React from 'react'
+import { DraggableCard } from '~/components/editor'
+import { activeBlocksAtom } from '~/store'
 
-import { SortableContext } from "@dnd-kit/sortable";
+import { SortableContext } from '@dnd-kit/sortable'
 
 export interface IEditorColumnTabPanelProps {}
 
 export function EditorColumnTabPanel(props: IEditorColumnTabPanelProps) {
-  const blockIds = useAtomValue(activeBlocksAtom);
+  const blockIds = useAtomValue(activeBlocksAtom)
+
   return (
     <SortableContext items={blockIds}>
       <div className="h-[74vh] overflow-auto border p-4 lg:h-[83vh]">
         <ul className="flex flex-col space-y-2">
           {blockIds.map((data, index) => (
-            <DraggableCard key={data} id={data} positon={index} />
+            <DraggableCard key={data} id={data} position={index} />
           ))}
         </ul>
       </div>
     </SortableContext>
-  );
+  )
 }
